04760cf60fd4803ed3aa57fccd100c922031a420
ChrisCamilo <53841709+ChrisCamilo@users.noreply.github.com>
2021-08-05 23:45:21 -0300

    feat: Properly handle login using database records and store in browser

--- src/pages/Logado/index.js
+++ src/pages/Logado/index.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import './styles.css';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 export default function Logado() {
-  const userName = localStorage.getItem('userName');
+  const [username, setUsername] = useState("");
+
+  useEffect(() =>{
+    async function getUsername() {
+      try {
+        const username = await AsyncStorage.getItem('username').then(value => {
+          setUsername(value);
+        });
+        return username;
+      } catch (err) {
+        return null;
+      }
+    }
+
+    getUsername();
+  })
 
   return (
-    <p>logado como {userName}</p>
+    <p>logado como {username}</p>
   );
 }
--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,33 +1,45 @@
 import React, { useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 import Logo from '../../assets/pombo.jpg';
 import api from '../../services/api'
 import './styles.css';
 
 export default function Logon() {
-  const [password, setPassword] = useState('');
-  const [email, setEmail] = useState('');
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
 
   const history = useHistory();
+
+  const storeData = async (key, value) => {
+    try {
+      await AsyncStorage.setItem(key, value);
+    } catch (e) {
+      alert("Login", "Houve um erro no login");
+    }
+  }
 
   async function handleSignIn(e) {
     e.preventDefault();
 
     const data = {
-      email,
-      password,
+      usuario,
+      senha
     };
 
-    try {
-      const response = await api.post('/session', data);  
-
-      localStorage.setItem('userName', response.data.name);
-      alert('Sessão iniciada');
-      history.push('/logado');
-    } catch (err) {
-      alert('Credenciais inválidas');
-    }
-
+    await api
+      .post('/acesso/login', data)
+      .then(async res => {
+        await storeData('token', res.data.token);
+        await storeData('username', usuario);
+        
+        alert('Sessão iniciada');
+        history.push('/logado');
+      }).catch(err => {
+        alert('Credenciais inválidas');
+        console.log(err);
+      });
   }
 
   return (
@@ -36,16 +48,16 @@ export default function Logon() {
         <form onSubmit={handleSignIn}>
           <img src={Logo} alt="LeColombe Royale logo" />
           <input
-            type="email"
-            placeholder="Endereço de e-mail"
-            value={email}
-            onChange={e => setEmail(e.target.value)}
+            type="text"
+            placeholder="Nome de usuário"
+            value={usuario}
+            onChange={e => setUsuario(e.target.value)}
           />
           <input
             type="password"
             placeholder="Senha"
-            value={password}
-            onChange={e => setPassword(e.target.value)}
+            value={senha}
+            onChange={e => setSenha(e.target.value)}
           />
           <button type="submit">Entrar</button>
           <hr />
