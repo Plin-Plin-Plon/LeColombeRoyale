d0bf9abbfafa2487c264184ba48c278034ed13d9
ChrisCamilo <53841709+ChrisCamilo@users.noreply.github.com>
2021-08-05 23:37:33 -0300

    feat: Add api default headers, change port to 8080 and add token handling

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,33 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 
-const api = axios.create({
-    baseURL: 'http://localhost:3333',
-})
+const api = () => {
+  const defaultOptions = {
+    baseURL: 'http://localhost:8080/',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  };
 
-export default api;
+  const getToken = async () => {
+    try {
+      const value = await AsyncStorage.getItem('token');
+      return value;
+    } catch (err) {
+      return null;
+    }
+  };
+
+  let instance = axios.create(defaultOptions);
+
+  instance.interceptors.request.use(async config => {
+    const token = await getToken();
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+    return config;
+  });
+
+  return instance;
+};
+
+export default api();
