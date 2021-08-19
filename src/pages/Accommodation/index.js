0c8c6b01184b4b1827c44531773984d5a6e469c2
ChrisCamilo <53841709+ChrisCamilo@users.noreply.github.com>
2021-08-19 18:47:04 -0300

    refactor: Add rooms list in select and api request to create accomodation

--- src/pages/Accommodation/index.js
+++ src/pages/Accommodation/index.js
@@ -1,32 +1,74 @@
-import React, { useState } from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import Logo from '../../assets/PC.png';
-import api from '../../services/api'
-import './styles.css';
+import React, { useState, useEffect } from 'react';
 import { cpfMask } from "masks-br";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Logo from '../../assets/PC.png';
+
+import history from "../../history";
+import api from '../../services/api'
+
+import './styles.css';
 
 export default function Login() {
-  const [quarto, setQuarto] = useState('');
+  const [quartoIndex, setQuartoIndex] = useState(0);
+  const [rooms, setRooms] = useState([]);
   const [hospede, setHospede] = useState('');
   const [datain, setDatain] = useState('');
   const [dataout, setDataout] = useState('');
-  const [value, setValue] = useState('');
 
-  const history = useHistory();
+  useEffect(() => {
+    async function fetchRooms() {
+      await api
+        .get("quarto/index")
+        .then(res => {
+          setRooms(res.data);
+        }).catch(err => {
+          console.log(err);
+        })
+    }
+
+    fetchRooms();
+  }, [])
+
+  async function syncLoadData(key) {
+    try {
+      const data = await AsyncStorage.getItem(key);
+      return data;
+    } catch (err) {
+      return null;
+    }
+  }
 
   async function handleRegister(e) {
     e.preventDefault();
 
+    const userId = await syncLoadData('user_id');
+    const room = rooms[quartoIndex];
+
     const data = {
-      quarto,
-      hospede,
-      datain,
-      dataout,
-      value
+      hospede: {
+        idPessoa: userId
+      },
+      quarto: {
+        numero: room.numero
+      },
+      dataChegada: datain,
+      dataSaida: dataout,
+      diaria: room.valor,
+      valorTotal: room.valor,
+      atual: true
     };
 
-    history.push('/home');
-
+    await api
+      .post("hospedagem/create", data)
+      .then(async res => {
+        await AsyncStorage.setItem('accomodation_id', res.data.idHospedagem);
+        await AsyncStorage.setItem('room', res.data.quarto.numero);
+        history.push("/home");
+      }).catch(async err => {
+        await AsyncStorage.removeItem('accomodation_id');
+        await AsyncStorage.removeItem('room');
+        alert('Houve um erro no cadastro da hospedagem');
+      })
   }
 
   return (
@@ -34,12 +76,13 @@ export default function Login() {
       <div className='Container'>
         <form onSubmit={handleRegister}>
           <img src={Logo} alt="LeColombe Royale logo" />
-          <input
-            type="number"
-            placeholder="Quarto"
-            value={quarto}
-            onChange={e => setQuarto(e.target.value)}
-          />
+          <select placeholder="Quarto" onChange={e => setQuartoIndex(e.target.value)}>
+            {rooms.map((quarto, index) => {
+              return (
+                <option key={quarto.numero} value={index}>Quarto: {quarto.numero}</option>
+              )
+            })}
+          </select>
           <input
             type="text"
             placeholder="CPF do hospede"
@@ -58,12 +101,6 @@ export default function Login() {
             placeholder="Data da saída"
             value={dataout}
             onChange={e => setDataout(e.target.value)}
-          />
-          <input
-            type="number"
-            placeholder="Valor da diária"
-            value={value}
-            onChange={e => setValue(e.target.value)}
           />
           <button type="submit">Cadastrar Hospedagem</button>
         </form>
