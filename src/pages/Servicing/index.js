05f13d987f488b92a7f53f39618d29bd02d5ce28
ChrisCamilo <53841709+ChrisCamilo@users.noreply.github.com>
2021-08-19 16:11:28 -0300

    feat: Add api request to create order

--- src/pages/Servicing/index.js
+++ src/pages/Servicing/index.js
@@ -1,29 +1,69 @@
 import React, { useEffect, useState } from 'react';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import api from '../../services/api'
 import Logout from '../../Components/Logout/Logout';
-import history from "../../history";
 
 import KitchenLogo from '../../assets/ratatuile.png'
 import './styles.css';
 
 export default function Servicing() {
+  const [loading, setLoading] = useState(true);
   const [services, setServices] = useState([]);
   const [codigo, setCodigo] = useState("");
 
   useEffect(() => {
-    async function fetchServices() {
-      await api
-        .get("servico/index")
-        .then(res => {
-          setServices(res.data);
-        }).catch(err => {
-          console.log(err);
-        });
+    if (loading) {
+      async function fetchServices() {
+        await api
+          .get("servico/index")
+          .then(res => {
+            setLoading(false);
+            setServices(res.data);
+          }).catch(err => {
+            console.log(err);
+          });
+      }
+
+      fetchServices();
+    }
+  }, [loading])
+
+  async function handleOrder() {
+    async function loadData(key) {
+      try {
+        const data = await AsyncStorage.getItem(key);
+        return data;
+      } catch (err) {
+        return null;
+      }
     }
 
-    fetchServices();
-  })
+    const idPessoa = await loadData('user_id');
+    const numero = await loadData('room');
+
+    const data = {
+      hospede: {
+        idPessoa
+      },
+      quarto: {
+        numero
+      },
+      servico: {
+        idServico: codigo
+      },
+      concluido: false
+    };
+
+    await api
+      .post("pedido/create", data)
+      .then(res => {
+        alert('Pedido criado com sucesso, aguarde a entrega do mesmo em seu quarto');
+      }).catch(err => {
+        alert('Houve um erro ao realizar o pedido, por favor tente novamente');
+        console.log(err);
+      })
+  }
 
   return (
     <div className="body">
@@ -37,18 +77,19 @@ export default function Servicing() {
         </header>
         <form>
           <input
-          type="text"
-          placeholder="Código do prato"
-          value={codigo}
-          onChange={e => setCodigo(e.target.value)}
+            type="text"
+            placeholder="Código do prato"
+            value={codigo}
+            onChange={e => setCodigo(e.target.value)}
           />
-          <button>Solicitar prato</button>
+          <button onClick={handleOrder} type="button">
+            Solicitar prato
+          </button>
         </form>
         <h1>Nossos pratos:</h1>
         <div className="listContainer">
           <ul>
             {services.length !== 0 ?
-
               services.map(services => (
                 <li key={services.idServico}>
                   <div className="title">
