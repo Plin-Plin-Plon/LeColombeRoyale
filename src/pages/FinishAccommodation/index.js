28e46ba0a497cbec7cffe2060fb2f426dc9655fc
ChrisCamilo <53841709+ChrisCamilo@users.noreply.github.com>
2021-08-20 05:41:08 -0300

    feat: Add api request to finish accommodation

--- src/pages/FinishAccommodation/index.js
+++ src/pages/FinishAccommodation/index.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import api from '../../services/api'
 import { cpfMask } from "masks-br";
+
+import history from "../../history";
+import api from '../../services/api'
+import Navbar from '../../Components/Navbar/Navbar';
 
 import Logo from '../../assets/PC.png';
 import './styles.css';
@@ -8,25 +11,63 @@ import './styles.css';
 export default function FinishAccommodation() {
   const [CPF, setCPF] = useState('');
 
-  async function handleFinish(e){
+  async function handleFinish(e) {
+    console.log("AQUI");
     e.preventDefault();
+
+    await api
+      .get(`hospede/index?cpf=${CPF}`)
+      .then(async res => {
+        if (res.data.message) {
+          alert(res.data.message);
+        } else {
+          await api
+            .get(`hospedagem/index?idHospede=${res.data.idPessoa}`)
+            .then(async res => {
+              if (res.data.message) {
+                alert(res.data.message);
+              } else {
+                await api
+                  .patch(`hospedagem/update?idHospedagem=${res.data.idHospedagem}&atual=${false}`)
+                  .then(async res => {
+                    if (res.data.message) {
+                      alert(res.data.message);
+                    } else {
+                      alert('Hospedagem finalizada com sucesso!');
+                      history.push("/kitchen");
+                    }
+                  }).catch(async err => {
+                    console.log(err);
+                    alert('Houve um erro na finalização da hospedagem');
+                  })
+              }
+            }).catch(async err => {
+              console.log(err);
+              alert('Erro na procura da hospedagem do hóspede especificado');
+            })
+        }
+      }).catch(async err => {
+        console.log(err);
+        alert('Erro na procura do hóspede, tente novamente');
+      })
   }
 
   return (
     <div className='body'>
-    <div className='Container'>
-      <form onSubmit={handleFinish}>
-        <img src={Logo} alt="LeColombe Royale logo" />
-        <input
-          type="text"
-          placeholder="CPF"
-          value={CPF}
-          onChange={e => setCPF(cpfMask(e.target.value))}
-        />
-        <button type="submit">Finalizar estadia</button>
-      </form>
+      <Navbar />
+      <div className='Container'>
+        <form onSubmit={handleFinish}>
+          <img src={Logo} alt="LeColombe Royale logo" />
+          <input
+            type="text"
+            placeholder="CPF"
+            value={CPF}
+            onChange={e => setCPF(cpfMask(e.target.value))}
+          />
+          <button type="submit">Finalizar estadia</button>
+        </form>
 
+      </div>
     </div>
-  </div>
-    );
+  );
 }
